e51d9b6207566db04410ad13592cb11cc4c4d15e
TechnoStudy <ismettechno@gmail.com>
2025-10-06 21:29:58 +0100

    Ürün ekleme testi yazıldı

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,5 +3,7 @@ package Gun01;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Login Testi kodları yapıldı");
+
+        System.out.println("ek kod yazıldı");
     }
 }
--- src/Gun01/Merhaba2.java
+++ src/Gun01/Merhaba2.java
@@ -1,4 +1,8 @@
 package Gun01;
 
 public class Merhaba2 {
+    public static void main(String[] args) {
+        System.out.println("ürün ekleme testi yazıldı.");
+
+    }
 }
